ae815c5b0e9e8bc47df859d806fe2fb5cba9ae34
windy003 <928920471@qq.com>
2026-02-12 07:14:50 +0800

    ok

--- run_screenshots_webserver.cmd
+++ run_screenshots_webserver.cmd
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-start /b pythonw "./app.py"
-=======
-start /b pythonw "D:\files\using\Web\pc_screenshot_webserver\app.py"
->>>>>>> 9b193c5d14e34943f639d6cbac5a97c1d46d8e96
+start  /b  pythonw  "D:\files\using\Web\pc_screenshot_webserver_AFMF2\app.py"
